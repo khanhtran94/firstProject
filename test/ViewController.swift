5560f944c3a26c0ff4fcd1f295f5cc49b5a0cbd7
khanhTran <khanhtrantung190894@gmail.com>
2020-11-30 21:23:08 +0700

    init project storyboard

---
+++ test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by water on 11/30/20.
+//  Copyright © 2020 water. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
